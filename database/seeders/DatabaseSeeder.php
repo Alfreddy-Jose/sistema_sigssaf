bda69620c514aea989ca1eab686cbc8ba7a256cb
Alfreddy Vargas <freddyvargas163@gmail.com>
2025-08-25 21:33:56 -0400

    Corriendo seeder AlvaroValero

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(SuperAdminSeeder::class);
         $this->call(AsistSeeder::class);
         $this->call(CoordinadorSeeder::class);
+        $this->call(AlvaroValeroSeeder::class);
         //$this->call(EstadisticaSeeder::class);
         //$this->call(JoseFernandoSeeder::class);
         // $this->call(SeederTablaPermisos::class);
